da61268bb1958e0140ce5c2a1992dc811ca93d06
crissanchezmora <76103659+crissanchezmora@users.noreply.github.com>
2021-03-11 19:28:48 +0100

    Add files via upload
    ¿Acabado?

--- assignacio.hpp
+++ assignacio.hpp
@@ -1,6 +1,7 @@
 #ifndef ASSIGNACIO_HPP
 #define ASSIGNACIO_HPP
 #include <iostream>
+
 using namespace std;
 
 class Assignacio{
@@ -9,28 +10,38 @@ class Assignacio{
 
 private:
 
+    vector <bool> boolAssig;
+    vector <char> charAssig;
 
 public:
 
 //Constructores
 Assignacio();
     //PRE: cierto.
-    //POST: el resultado es una assignación sin variables.
+    //POST: el resultado es una asignacion sin variables.
 
 Assignacio (int n);
-    //PRE; cierto.
-    //POST: el resultado es una asignacion con n variables.
+    //PRE: cierto.
+    //POST: el resultado es una asignacion con n variables en orden alfabetico.
 
 //Destructor
 ~Assignacio();
-/* Post: esborra automaticament els objectes locals en sortir d'un ambit de visibilitat */
+    /* Post: borra automaticamente los objetos locales al salir de un ambito de visibilidad */
 
 //Modificadores
-Assignacio operator++(const Assignacio &a);
+void setVariables (vector <char> vars);
+    //PRE: cierto.
+    //POST: modifica un vector <char> vacio, añadiendo las variables de la asignación.
+            //vector={a,b,c,d,···,z}
+void operator++(const Assignacio &a);
     /* Pre: cierto*/
-    /* Post: el resultado es la siguiente asignacion (orden total)*/
+    /* Post: calcula la siguiente asignacion (orden total)*/
 
 //Consultores
+bool getVariable (char var);
+    //PRE: cierto.
+    //POST: devuelve el valor bool correspondiente a la variable.
+            //input (a) = output (false)
 
 
 
