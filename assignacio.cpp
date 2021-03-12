2c3cddf7e2426da9756853a430653b1400b40a8d
freeman35g <gabriel.guardiola1999@gmail.com>
2021-03-12 02:16:18 +0100

    v2.0
    Changelog:
     - Depurada y terminada la clase Assignacio.

--- assignacio.cpp
+++ assignacio.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <bitset>
+#include <math.h>
 using namespace std;
 
 //Constructores
@@ -17,10 +18,11 @@ Assignacio:: Assignacio(){
 Assignacio:: Assignacio (int n){
     //PRE: cierto.
     //POST: el resultado es una asignacion con n variables en orden alfabetico.
-
-    vector <bool> boolAssig(n,false);
-    vector <char> charAssig(n);
-    int it=0;
+    for (int i=0; i<n;i++){
+        boolAssig.push_back(false);
+    }    
+    it=0;
+    
 
 }
 //Destructor
@@ -29,48 +31,70 @@ Assignacio::~Assignacio(){
 
 }
 //Modificadores
-void Assignacio:: setVariables (vector <char> vars){
+void Assignacio::setVariables (vector<char> vars){
     //PRE: cierto.
     //POST: modifica un vector <char> vacio, añadiendo las variables de la asignación.
             //vector={a,b,c,d,···,z}
-
+    
     for (int i=0; i<int(vars.size());i++){
-        charAssig[i]=vars[i];
+        charAssig.push_back(vars[i]);
     }
 
 }
-void Assignacio:: operator++(const Assignacio &a){
-    /* Pre: cierto*/
-    /* Post: calcula la siguiente asignacion (orden total:empezando por la ultima variable)*/
 
-    
-    bitset <boolAssig.size()> ordre(it);
-        
+void Assignacio::resetAssig (){
+    //PRE: cierto.
+    //POST: reinicia el iterador it para reiniciar las asignaciones binarias (todo a false y listo para volver a operar las combinaciones desde 0).
+    //ej: false, false, false, true, true | it = 3 -------- false, false, false, false, false | it = 0;
+    it = 0;
     for (int i=0; i<int(boolAssig.size());i++){
         boolAssig[i]=false;
-    }
-
-    for (int i=0; i<int(boolAssig.size());i++){
-        if (ordre[i]==1) boolAssig[i]=true;
-    }
-
+    }  
+}
+void Assignacio::operator++() {
+    /* Pre: cierto*/
+    /* Post: calcula la siguiente asignacion (orden total:empezando por la ultima variable)*/
     it++;
+    bitset <26> ordre(it);//lo lee alreves!!
+    for (int i=0; i<int(boolAssig.size());i++){
+        boolAssig[i]=false;
+    }  
+    int i = int(boolAssig.size())-1;
+    int j = 0;
+    while(i>=0){
+        if(ordre[j] == 1)boolAssig[i] = true;          
+        i--;
+        j++;
+    }       
 
 }
 //Consultores
-bool Assignacio:: getVariable (char var){
+bool Assignacio::getVariable (char var){
     //PRE: cierto.
     //POST: devuelve el valor bool correspondiente a la variable.
             //input (a) = output (false)
 
     bool found=false;
     int i=0;
-    while (not found and i<int(charAssig.size()){
+
+    while (i<int(charAssig.size()) and not found){
+
         if(charAssig[i]==var){
-            found=false;
+            found=true;
         }
+        i++;
     }
 
-    return boolAssig[i];
+    return boolAssig[i-1];
+
+}
+
+bool Assignacio::isFinal (){
+    //PRE: cierto.
+    //POST: devuelve true si el objeto se encuentra en la asignacion final (todas true)
+
+    fin=false;
+    if(it == pow(2, int(boolAssig.size()))) fin = true;
+    return fin;
 
 }
--- assignacio.hpp
+++ assignacio.hpp
@@ -2,7 +2,7 @@
 #define ASSIGNACIO_HPP
 #include <iostream>
 #include <bitset>
-
+#include <vector>
 using namespace std;
 
 class Assignacio{
@@ -14,7 +14,7 @@ private:
     vector <bool> boolAssig;
     vector <char> charAssig;
     int it; 
-               
+    bool fin;  
 public:
 
 //Constructores
@@ -35,7 +35,13 @@ void setVariables (vector <char> vars);
     //PRE: cierto.
     //POST: modifica un vector <char> vacio, añadiendo las variables de la asignación.
             //vector={a,b,c,d,···,z}
-void operator++(const Assignacio &a);
+
+void resetAssig ();
+    //PRE: cierto.
+    //POST: reinicia el iterador it para reiniciar las asignaciones binarias (todo a false y listo para volver a operar las combinaciones desde 0).
+    //ej: false, false, false, true, true | it = 3 -------- false, false, false, false, false | it = 0;
+
+void operator ++(); 
     /* Pre: cierto*/
     /* Post: calcula la siguiente asignacion (orden total:empezando por la ultima variable)*/
 
@@ -44,9 +50,12 @@ bool getVariable (char var);
     //PRE: cierto.
     //POST: devuelve el valor bool correspondiente a la variable.
             //input (a) = output (false)
+bool isFinal ();
+    //PRE: cierto.
+    //POST: devuelve true si el objeto se encuentra en la asignacion final (todas true)
 
 
 
 
-}
+};
 #endif
